e6bfc818ba9b68fe56c15f560c7d6fd79d86658e
Alex Karpenkov <sasha.karpenkov.34@gmail.com>
2023-09-19 12:41:55 +0300

    update sitemap logic

--- utils/Sitemap/Sitemap.action.js
+++ utils/Sitemap/Sitemap.action.js
@@ -17,22 +17,22 @@ import {
     EXTERNAL_DATA_URL_BLOG_TAGS_API
 } from "@/utils/Sitemap/Sitemap.constant";
 
-export const requestMainPage = await fetch(EXTERNAL_DATA_URL_MAIN_PAGE_API);
-export const requestProjectsPage = await fetch(EXTERNAL_DATA_URL_PROJECTS_PAGE_API);
-export const requestBlogPage = await fetch(EXTERNAL_DATA_URL_BLOG_PAGE_API);
-export const requestNewsPage = await fetch(EXTERNAL_DATA_URL_NEWS_PAGE_API);
-export const requestFAQPage = await fetch(EXTERNAL_DATA_URL_FAQ_PAGE_API);
-export const requestPolicyPage = await fetch(EXTERNAL_DATA_URL_POLICY_PAGE_API);
-export const requestContactsPage = await fetch(EXTERNAL_DATA_URL_CONTACTS_PAGE_API);
-export const requestCompanyPage = await fetch(EXTERNAL_DATA_URL_COMPANY_PAGE_API);
-export const requestProductionPage = await fetch(EXTERNAL_DATA_URL_PRODUCTION_PAGE_API);
+export const requestMainPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_MAIN_PAGE_API}/${lang}`);
+export const requestProjectsPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_PROJECTS_PAGE_API}/${lang}`);
+export const requestBlogPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_BLOG_PAGE_API}/${lang}`);
+export const requestNewsPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_NEWS_PAGE_API}/${lang}`);
+export const requestFAQPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_FAQ_PAGE_API}/${lang}`);
+export const requestPolicyPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_POLICY_PAGE_API}/${lang}`);
+export const requestContactsPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_CONTACTS_PAGE_API}/${lang}`);
+export const requestCompanyPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_COMPANY_PAGE_API}/${lang}`);
+export const requestProductionPage = async (lang) => await fetch(`${EXTERNAL_DATA_URL_PRODUCTION_PAGE_API}/${lang}`);
 
 /* requests for dynamic pages */
-export const requestProjects = await fetch(EXTERNAL_DATA_URL_PROJECTS_API);
-export const requestBlog = await fetch(EXTERNAL_DATA_URL_BLOG_API);
-export const requestNews = await fetch(EXTERNAL_DATA_URL_NEWS_API);
+export const requestProjects = async (lang) => await fetch(`${EXTERNAL_DATA_URL_PROJECTS_API}/${lang}`);
+export const requestBlog = async (lang) => await fetch(`${EXTERNAL_DATA_URL_BLOG_API}/${lang}`);
+export const requestNews = async (lang) => await fetch(`${EXTERNAL_DATA_URL_NEWS_API}/news/${lang}`);
 
 /* requests for tags on pages */
-export const requestProjectsTags = await fetch(EXTERNAL_DATA_URL_PROJECTS_TAGS_API);
-export const requestNewsTags = await fetch(EXTERNAL_DATA_URL_NEWS_TAGS_API);
-export const requestBlogTags = await fetch(EXTERNAL_DATA_URL_BLOG_TAGS_API);
+export const requestProjectsTags = async (lang) => await fetch(`${EXTERNAL_DATA_URL_PROJECTS_TAGS_API}/${lang}`);
+export const requestNewsTags = async (lang) => await fetch(`${EXTERNAL_DATA_URL_NEWS_TAGS_API}/${lang}`);
+export const requestBlogTags = async (lang) => await fetch(`${EXTERNAL_DATA_URL_BLOG_TAGS_API}/${lang}`);
