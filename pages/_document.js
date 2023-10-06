77cb507b8e0a9d31f5250239302f42e06b5eac20
Alex Karpenkov <sasha.karpenkov.34@gmail.com>
2023-10-06 15:19:51 +0300

    feat: bx chat

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ export default function Document({ ...props }) {
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-TNK7DJKQ');`}}></script>
+            <script dangerouslySetInnerHTML={{ __html: `(function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://bx.1ak-group.com/upload/crm/site_button/loader_1_1cfjjz.js');` }}></script>
             <meta name="robots" content="index, follow"/>
             <meta name="yandex-verification" content="37a94647988229ec" />
         </Head>
