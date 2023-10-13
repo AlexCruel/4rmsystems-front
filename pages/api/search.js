c4a58445689be8f1019aff1906bb2b27db6e66ef
Alex Karpenkov <sasha.karpenkov.34@gmail.com>
2023-10-13 11:20:55 +0300

    fix api

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,4 +1,5 @@
-const url = process.env.NEXT_PUBLIC_API_URL;
+//const url = process.env.NEXT_PUBLIC_API_URL;
+const url = "https://api.4rm.org/api";
 
 export const getSearchInfo = async (searchData, lang) => {
     const request = await fetch(`${url}/search/${searchData}/${lang}`);
