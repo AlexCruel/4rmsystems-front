2dba2deaba5fb90a2f7651b797abaabfab89861e
Alex Karpenkov <sasha.karpenkov.34@gmail.com>
2023-11-01 15:21:35 +0300

    upd: tag disable

--- components/Tags/index.js
+++ components/Tags/index.js
@@ -1,7 +1,13 @@
 import cn from "./styles.module.scss";
 import Link from "next/link";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
 
 const Tags = ({ type, tags }) => {
+    const router = useRouter();
+    const isTagPath = router.route.includes('tag');
+    const tag_id = router.query.tag_id;
+
     return (
         <div className={cn.container}>
             <div className={cn.container__tags}>
@@ -10,7 +16,7 @@ const Tags = ({ type, tags }) => {
                         return (
                             <li key={index}>
                                 <Link href={`/${type}/tag/${item.slug}`}>
-                                    <button>{item.name}</button>
+                                    <button className={tag_id !== item.slug && isTagPath  ? cn.tags_disable : ''}>{item.name}</button>
                                 </Link>
                             </li>
                         );
